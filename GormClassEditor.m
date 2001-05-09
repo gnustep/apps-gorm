176f790bc3c50f2317479808b43617849052c670
Richard Frith-Macdonald <rfm@gnu.org>
2001-05-09 08:15:58 +0000

    Added missed file
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/apps/gorm/trunk@9907 72102866-910b-0410-8b05-ffd578937521

---
+++ GormClassEditor.m
@@ -0,0 +1,66 @@
+#import "Gorm.h"
+#import "GormPrivate.h"
+
+@implementation	GormClassEditor
+
+- (GormClassEditor*) initWithDocument: (GormDocument*)doc
+{
+  self = [super init];
+  if (self != nil)
+    {
+      document = doc; // loose connection
+    }
+  return self;
+}
+
+- (void) dealloc 
+{
+  RELEASE(selectedClassName);
+
+  [super dealloc];
+}
+
++ (GormClassEditor*) classEditorForDocument: (GormDocument*)doc
+{
+  return AUTORELEASE([[self alloc] initWithDocument: doc]);
+}
+
+- (void) setSelectedClassName: (NSString*)cn
+{
+  ASSIGN(selectedClassName, cn);
+}
+
+//--- IBSelectionOwners protocol ---
+- (unsigned) selectionCount
+{
+  return (selectedClassName == nil)?0: 1;
+}
+
+- (NSArray*) selection
+{
+  // when asked for a selection, it returns a class proxy
+  if (selectedClassName != nil) 
+    {
+      NSArray		*array;
+      GormClassProxy	*classProxy;
+
+      classProxy = [[GormClassProxy alloc] initWithClassName:
+	selectedClassName];
+      array = [NSArray arrayWithObject: classProxy];
+      RELEASE(classProxy);
+      return array;
+    } 
+  else
+    {
+      return [NSArray array];
+    }
+}
+
+- (void) drawSelection
+{
+
+}
+
+@end
+
+
