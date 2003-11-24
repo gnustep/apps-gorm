b8bac8c77a5572b62ea5971ee71e8c669db3bf9e
Gregory John Casamento <greg.casamento@gmail.com>
2003-11-24 04:08:41 +0000

    Version 0.5.0
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/apps/gorm/trunk@18132 72102866-910b-0410-8b05-ffd578937521

--- Gorm.m
+++ Gorm.m
@@ -519,9 +519,9 @@ NSString *GormWillDetachObjectFromDocumentNotification = @"GormWillDetachObjectF
      forKey: @"ApplicationName"];
   [dict setObject: @"[GNUstep | Graphical] Object Relationship Modeller"
      forKey: @"ApplicationDescription"];
-  [dict setObject: @"Gorm 0.4.10 (Beta)" 
+  [dict setObject: @"Gorm 0.5.0 (Beta)" 
      forKey: @"ApplicationRelease"];
-  [dict setObject: @"0.4.10 Nov 23 2003" 
+  [dict setObject: @"0.5.0 Nov 23 2003" 
      forKey: @"FullVersionID"];
   [dict setObject: [NSArray arrayWithObjects: @"Gregory John Casamento <greg_casamento@yahoo.com>",
 			 @"Richard Frith-Macdonald <rfm@gnu.org>",
