adc5d81e9147d697adf82beb622be4a343e84a88
Gregory John Casamento <greg.casamento@gmail.com>
2008-05-17 00:46:36 +0000

    Added plugin class.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/apps/gorm/trunk@26532 72102866-910b-0410-8b05-ffd578937521

---
+++ GormCore/GormPlugin.h
@@ -0,0 +1,34 @@
+/* GormNibModule.m
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc.
+ *
+ * Author:	Gregory John Casamento <greg_casamento@yahoo.com>
+ * Date:	2007
+ * 
+ * This file is part of GNUstep.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ */
+
+#ifndef GORM_GORMPLUGIN
+#define GORM_GORMPLUGIN
+#include <InterfaceBuilder/IBPlugin.h>
+
+@interface GormPlugin : IBPlugin
+- (void) registerDocumentTypeName: (NSString *)name
+                humanReadableName: (NSString *)hrName
+                    forExtensions: (NSArray *)extensions;
+@end
+
+#endif
