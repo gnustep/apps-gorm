b2bbde68269afd0d379c22b83bf920838740cea3
Gregory John Casamento <greg.casamento@gmail.com>
2008-06-23 22:47:01 +0000

    Remove ;
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/apps/gorm/trunk@26692 72102866-910b-0410-8b05-ffd578937521

--- Plugins/Nib/GormNibPlugin.m
+++ Plugins/Nib/GormNibPlugin.m
@@ -32,7 +32,7 @@
 @implementation GormNibPlugin
 - (void) didLoad
 {
-  [self registerDocumentTypeName: @"GSNibFileType";
+  [self registerDocumentTypeName: @"GSNibFileType"
 	humanReadableName: @"Cocoa Nib"
 	forExtensions: [NSArray arrayWithObjects: @"nib",nil]];
 }
