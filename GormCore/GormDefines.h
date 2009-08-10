e40c813e228ffc0ffa604f19beb5fc59960013ee
Gregory John Casamento <greg.casamento@gmail.com>
2009-08-10 14:02:19 +0000

    * GormCore/GormDefines.h: Move defines for certain things here 	so that they are centralized.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/apps/gorm/trunk@28447 72102866-910b-0410-8b05-ffd578937521

---
+++ GormCore/GormDefines.h
@@ -0,0 +1,36 @@
+/* GormDefines.h
+ *
+ * Copyright (C) 2009 Free Software Foundation, Inc.
+ *
+ * Author:	Gregory Casamento <greg.casamento@gmail.com>
+ * Date:	2009
+ * 
+ * This file is part of GNUstep.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111 USA.
+ */
+
+#ifndef	INCLUDED_GormDefines_h
+#define	INCLUDED_GormDefines_h
+
+#ifndef max
+#define max(a,b) ((a) >= (b) ? (a):(b))
+#endif
+
+#ifndef min
+#define min(a,b) ((a) <= (b) ? (a):(b))
+#endif
+
+#endif // GormDefines
