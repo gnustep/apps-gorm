04554d25c84f5d45a53da41f50afca84fecd73fd
David Chisnall <David.Chisnall@microsoft.com>
2019-02-03 09:23:14 +0000

    Fix accidental variable shadowing.
    We were looking up the ivar, but then not using the result, so got null
    pointer dereferences later.
    
    This should have been caught by normal compiler warnings, but apparently
    Gorm turns them off.

--- GormCore/GormDocument.m
+++ GormCore/GormDocument.m
@@ -105,7 +105,7 @@
   static Ivar iv;
   if (!iv)
     {
-      Ivar iv = class_getInstanceVariable([NSDocument class], "_window");
+      iv = class_getInstanceVariable([NSDocument class], "_window");
       NSAssert(iv, @"Unable to find _window ivar in NSDocument class");
     }
   return object_getIvar(self, iv);
