d16b9550a00df499c6447487c7e44e52f36f021d
Gregory Casamento <greg.casamento@gmail.com>
2021-07-14 08:51:00 -0400

    Update dependencies.

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -4,38 +4,6 @@ set -ex
 
 echo "Building..."
 
-# sudo apt-get -qq update
-# sudo apt-get install -y cmake pkg-config libgnutls28-dev libgmp-dev libffi-dev libicu-dev \
-#      libxml2-dev libxslt1-dev libssl-dev libavahi-client-dev zlib1g-dev
-
-# if [ $LIBRARY_COMBO = 'gnu-gnu-gnu' ];
-# then
-#   if [ $CC = 'gcc' ];
-#   then
-#     sudo apt-get install -y gobjc;
-#   fi;
-#   sudo apt-get install -y libobjc-8-dev libblocksruntime-dev;
-# else
-#   curl -s -o - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -;
-#   sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main" && sudo apt-get update -qq;
-#   sudo apt-get install -y clang-9 libkqueue-dev libpthread-workqueue-dev;
-#   sudo update-alternatives   --install /usr/bin/clang   clang   /usr/bin/clang-9   10 \
-#                              --slave   /usr/bin/clang++ clang++ /usr/bin/clang++-9;
-#   export PATH=$(echo "$PATH" | sed -e 's/:\/usr\/local\/clang-7.0.0\/bin//');
-#   if [ "$RUNTIME_VERSION" = "gnustep-2.0" ];
-#   then
-#      sudo update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin/ld.gold" 10;
-#   fi;
-# fi;
-
-# if [ $LIBRARY_COMBO = 'ng-gnu-gnu' ];
-# then
-#   curl -LO https://cmake.org/files/v3.15/cmake-3.15.5-Linux-x86_64.tar.gz;
-#   tar xf cmake-3.15.5-Linux-x86_64.tar.gz;
-#   mv cmake-3.15.5-Linux-x86_64 $HOME/cmake;
-#   export PATH=$HOME/cmake/:$HOME/cmake/bin:$PATH
-# fi;
-
 # build dependencies...
 ./.github/scripts/dependencies.sh
 
--- .github/scripts/dependencies.sh
+++ .github/scripts/dependencies.sh
@@ -5,6 +5,39 @@ set -ex
 DEP_SRC=$HOME/dependency_source/
 DEP_ROOT=$HOME/staging
 
+install_prerequisites() {
+    sudo apt-get -qq update
+    sudo apt-get install -y cmake pkg-config libgnutls28-dev libgmp-dev libffi-dev libicu-dev \
+	 libxml2-dev libxslt1-dev libssl-dev libavahi-client-dev zlib1g-dev
+
+    if [ $LIBRARY_COMBO = 'gnu-gnu-gnu' ];
+    then
+	if [ $CC = 'gcc' ];
+	then
+	  sudo apt-get install -y gobjc;
+	fi;
+        sudo apt-get install -y libobjc-8-dev libblocksruntime-dev;
+    else
+	curl -s -o - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -;
+        sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main" && sudo apt-get update -qq;
+	sudo apt-get install -y clang-9 libkqueue-dev libpthread-workqueue-dev;
+	sudo update-alternatives   --install /usr/bin/clang   clang   /usr/bin/clang-9   10 \
+             --slave   /usr/bin/clang++ clang++ /usr/bin/clang++-9;
+	export PATH=$(echo "$PATH" | sed -e 's/:\/usr\/local\/clang-7.0.0\/bin//');
+        if [ "$RUNTIME_VERSION" = "gnustep-2.0" ];
+	then
+            sudo update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin/ld.gold" 10;
+	fi;
+    fi;
+    if [ $LIBRARY_COMBO = 'ng-gnu-gnu' ];
+    then
+	curl -LO https://cmake.org/files/v3.15/cmake-3.15.5-Linux-x86_64.tar.gz;
+	tar xf cmake-3.15.5-Linux-x86_64.tar.gz;
+	mv cmake-3.15.5-Linux-x86_64 $HOME/cmake;
+	export PATH=$HOME/cmake/:$HOME/cmake/bin:$PATH
+    fi;
+}
+
 install_gnustep_make() {
     cd $DEP_SRC
     git clone https://github.com/gnustep/tools-make.git
@@ -95,6 +128,7 @@ then
     install_libdispatch
 fi
 
+install_prerequisites
 install_gnustep_make
 install_gnustep_base
 install_gnustep_gui
