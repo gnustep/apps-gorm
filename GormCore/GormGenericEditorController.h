ecd126aa18a3004d2d439a7e2e12c503c10d2cc9
Gregory John Casamento <greg.casamento@gmail.com>
2022-11-12 13:01:56 -0500

    Add new gorm file and controller

---
+++ GormCore/GormGenericEditorController.h
@@ -0,0 +1,35 @@
+/* GormGenericEditor.h
+ *
+ * Copyright (C) 1999, 2003 Free Software Foundation, Inc.
+ *
+ * Author:	Richard Frith-Macdonald <richard@brainstrom.co.uk>
+ * Author:	Gregory John Casamento <greg_casamento@yahoo.com>
+ * Date:	1999, 2003, 2004
+ * 
+ * This file is part of GNUstep.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111 USA.
+ */
+
+#ifndef INCLUDED_GormGenericEditorController_h
+#define INCLUDED_GormGenericEditorController_h
+
+#import <Cocoa/Cocoa.h>
+#import <InterfaceBuilder/InterfaceBuilder.h>
+
+@interface GormGenericEditorController : NSViewController
+@end
+
+#endif
