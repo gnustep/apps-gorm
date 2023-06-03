bc6e15abb738b88115e64b5a0dbee657b49e9406
Gregory John Casamento <greg.casamento@gmail.com>
2023-06-03 19:34:09 -0400

    Add tools and update GormCode.h

---
+++ Tools/gormtool/main.m
@@ -0,0 +1,12 @@
+// main.m
+
+#import <Foundation/Foundation.h>
+
+#import <GormCore/GormCore.h>
+#import <InterfaceBuilder/InterfaceBuilder.h>
+
+int main(int argc, char **argv)
+{
+  puts("-- gormtool");
+  return 0;
+}
