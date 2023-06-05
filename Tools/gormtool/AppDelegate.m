3f0f8b13a0c31a3320e863f0b41ffaf8a257f139
Gregory John Casamento <greg.casamento@gmail.com>
2023-06-05 05:06:03 -0400

    Quit application in applicationDidFinishLaunching:

--- Tools/gormtool/AppDelegate.m
+++ Tools/gormtool/AppDelegate.m
@@ -1,11 +1,17 @@
-#import "AppDelegate.h"
 #import <Foundation/NSNotification.h>
+#import <Foundation/NSProcessInfo.h>
+
+#import "AppDelegate.h"
 
 @implementation AppDelegate
 
 - (void) applicationDidFinishLaunching: (NSNotification *)n
 {
   NSLog(@"== gormtool");
+
+  NSLog(@"processInfo: %@", [NSProcessInfo processInfo]);
+  
+  [NSApp terminate: nil];
 }
 
 @end
