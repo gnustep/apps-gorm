4a44cce1f0b7798d388e1448c4be1e3173c3f778
Gregory John Casamento <greg.casamento@gmail.com>
2023-06-05 05:50:08 -0400

    Add code to call setIsInInterfaceBuilder:

--- Tools/gormtool/AppDelegate.m
+++ Tools/gormtool/AppDelegate.m
@@ -1,9 +1,19 @@
 #import <Foundation/NSNotification.h>
 #import <Foundation/NSProcessInfo.h>
+#import <GNUstepGUI/GSNibLoading.h>
 
 #import "AppDelegate.h"
 
 @implementation AppDelegate
+
+- (void) process
+{
+  [NSClassSwapper setIsInInterfaceBuilder: YES];
+
+  NSLog(@"Processing...");
+  
+  [NSClassSwapper setIsInInterfaceBuilder: NO];
+}
 
 - (void) applicationDidFinishLaunching: (NSNotification *)n
 {
