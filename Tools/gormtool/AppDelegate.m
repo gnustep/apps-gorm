96132510fb96dd3e04b7516eca0d5843801af358
Gregory John Casamento <greg.casamento@gmail.com>
2023-06-08 23:00:14 -0400

    Move plugins into core

--- Tools/gormtool/AppDelegate.m
+++ Tools/gormtool/AppDelegate.m
@@ -1,16 +1,34 @@
+#import <Foundation/NSDictionary.h>
 #import <Foundation/NSNotification.h>
 #import <Foundation/NSProcessInfo.h>
+
 #import <GNUstepGUI/GSNibLoading.h>
 
 #import "AppDelegate.h"
 
 @implementation AppDelegate
 
+- (NSDictionary *) buildDictionary
+{
+  NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+  NSArray *keys = [dict allKeys];
+  /*
+  FOR_IN(NSString*, k, keys)
+    {
+      
+    }
+  END_FOR_IN(keys);
+  */
+  
+  return dict;
+}
+
 - (void) process
 {
+  NSDictionary *args = [self buildDictionary];
   [NSClassSwapper setIsInInterfaceBuilder: YES];
 
-  NSLog(@"Processing...");
+  NSLog(@"Processing... %@", args);
   
   [NSClassSwapper setIsInInterfaceBuilder: NO];
 }
