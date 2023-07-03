abb6dc553cb2db41cb1f71a8193df1eddcae9a08
Gregory John Casamento <greg.casamento@gmail.com>
2023-07-02 23:00:45 -0400

    Pull private methods into a separate file

--- Tools/gormtool/AppDelegate.m
+++ Tools/gormtool/AppDelegate.m
@@ -23,91 +23,8 @@
  * USA.
  */
 
-#import <Foundation/Foundation.h>
-#import <AppKit/AppKit.h>
-
-#import <GormCore/GormPlugin.h>
-#import <GormCore/GormDocumentController.h>
-
-#import <GNUstepGUI/GSNibLoading.h>
-
+#import "GormToolPrivate.h"
 #import "AppDelegate.h"
-
-static NSMutableArray *__types = nil;
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wobjc-protocol-method-implementation"
-
-// Special method category smashes so that we can register types...
-
-@implementation NSDocumentController (ToolPrivate)
-
-- (Class) documentClassForType: (NSString *)type
-{
-  return [GormDocument class];
-}
-
-- (NSString *) typeFromFileExtension: (NSString *)fileExtension
-{
-  int i, count = [__types count];
-
-  // Check for a document type with the supplied extension
-  for (i = 0; i < count; i++)
-    {
-      NSDictionary *typeInfo = [__types objectAtIndex: i];
-      NSArray *array = [typeInfo objectForKey: @"NSUnixExtensions"];
-
-      NSDebugLog(@"typeInfo = %@", typeInfo);
-      NSDebugLog(@"fileExtension = %@", fileExtension);
-      
-      if ([array containsObject: fileExtension])
-	{
-	  NSString *type = [typeInfo objectForKey: @"NSName"];
-	  NSDebugLog(@"type = %@", type);
-	  return type;
-	}
-    }
-
-  NSDebugLog(@"FAILED");
-  return nil;
-}
-
-@end
-
-@implementation GormDocument (ToolPrivate)
-
-+ (BOOL) isNativeType: (NSString *)type
-{
-  return YES;
-}
-
-@end
-
-@implementation GormPlugin (ToolPrivate)
-
-- (void) registerDocumentTypeName: (NSString *)name
-                humanReadableName: (NSString *)hrName
-                    forExtensions: (NSArray *)extensions
-{
-  if (__types == nil)
-    {
-      __types = [NSMutableArray arrayWithCapacity: 10];
-    }
-  
-  NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithObjectsAndKeys:
-						     name, @"NSName",
-						   hrName, @"NSHumanReadableName",
-						   extensions, @"NSUnixExtensions",
-						   @"Editor", @"NSRole",
-						   nil];
-  [__types addObject: dict];
-
-  NSDebugLog(@"__types = %@", __types);
-}
-
-@end
-
-#pragma GCC diagnostic pop
 
 @interface ArgPair : NSObject <NSCopying>
 {
@@ -177,14 +94,6 @@ static NSMutableArray *__types = nil;
 
 // AppDelegate...
 @implementation AppDelegate
-
-+ (void) initialize
-{
-  if (__types == nil)
-    {
-      __types = [NSMutableArray arrayWithCapacity: 10];
-    }
-}
 
 - (NSDictionary *) parseArguments
 {
