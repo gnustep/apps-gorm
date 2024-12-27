6c60a0e4b9b9650a73ab1ebeb87dd323760d76af
Gregory John Casamento <greg.casamento@gmail.com>
2024-12-27 04:03:50 -0500

    Update property

--- GormObjCHeaderParser/OCProperty.h
+++ GormObjCHeaderParser/OCProperty.h
@@ -1,35 +1,45 @@
-/* OCMethod.h
- *
- * Copyright (C) 1999 Free Software Foundation, Inc.
- *
- * Author:	Gregory John Casamento <greg_casamento@yahoo.com>
- * Date:	1999, 2002
- *
- * This file is part of GNUstep.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
+/* Definition of class OCProperty
+   Copyright (C) 2024 Free Software Foundation, Inc.
+   
+   By: Gregory John Casamento
+   Date: 27-12-2024
 
-#include <Foundation/Foundation.h>
+   This file is part of GNUstep.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110 USA.
+*/
 
-#include "GormObjCHeaderParser/OCIVar.h"
+#ifndef _OCProperty_h_INCLUDE
+#define _OCProperty_h_INCLUDE
 
-#ifndef INCLUDED_OCProperty_h
-#define INCLUDED_OCProperty_h
+#import "OCIVar.h"
+#import <GNUstepBase/GSVersionMacros.h>
 
+#if	defined(__cplusplus)
+extern "C" {
+#endif
+
+GS_EXPORT_CLASS
 @interface OCProperty : OCIVar
+
 @end
 
+#if	defined(__cplusplus)
+}
 #endif
+
+#endif	/* _OCProperty_h_INCLUDE */
+
