a31276009ccffac20d4a9765f894e7c0b42be779
Gregory John Casamento <greg.casamento@gmail.com>
2025-11-08 07:27:13 -0500

    Update toolbar palette

---
+++ Applications/Gorm/Palettes/5Toolbar/GormNSToolbar.h
@@ -0,0 +1,42 @@
+/* GormNSToolbar
+
+   Copyright (C) 2025 Free Software Foundation, Inc.
+
+   Author: Gregory Casamento <greg.casamento@gmail.com>
+   Date: Nov 2025
+   
+   This file is part of GNUstep.
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111 USA.
+*/
+
+/* All rights reserved */
+
+#ifndef GormNSToolbar_H_INCLUDE
+#define GormNSToolbar_H_INCLUDE
+
+#import <AppKit/NSToolbar.h>
+#import "GormToolbarDelegateProtocol.h"
+
+@class NSMutableArray;
+
+@interface GormNSToolbar : NSToolbar <NSToolbarDelegate, GormToolbarDelegateProtocol>
+{
+  NSMutableArray *_allowedItems;
+  NSMutableArray *_defaultItems;
+}
+@end
+
+#endif
