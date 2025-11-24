b9cd5a5979664f99dbe2e05037540c68b4e934dd
Gregory John Casamento <greg.casamento@gmail.com>
2025-11-24 11:44:21 -0500

    Add dragging methods

--- Applications/Gorm/Palettes/5Toolbar/GormToolbarEditor.m
+++ Applications/Gorm/Palettes/5Toolbar/GormToolbarEditor.m
@@ -97,6 +97,7 @@
 
 - (BOOL) acceptsTypeFromArray: (NSArray *)types
 {
+  NSLog(@"types = %@", types);
   // Accept link types for making connections (outlets, actions, etc.)
   if ([types containsObject: GormLinkPboardType])
     {
@@ -109,23 +110,106 @@
 - (NSDragOperation) draggingEntered: (id<NSDraggingInfo>)sender
 {
   NSPasteboard *pb = [sender draggingPasteboard];
-  
+  NSLog(@"Entered");
   // Check if this is a connection drag
   if ([pb availableTypeFromArray: [NSArray arrayWithObject: GormLinkPboardType]])
     {
+      NSLog(@"Success");
       return NSDragOperationLink;
     }
   
   return NSDragOperationNone;
 }
 
+- (void) draggingExited: (id<NSDraggingInfo>)sender
+{
+  NSPasteboard	*dragPb;
+  NSArray	*types;
+  NSRect         rect;
+
+  dragPb = [sender draggingPasteboard];
+  types = [dragPb types];
+  
+  if ([types containsObject: GormLinkPboardType] == YES)
+    {
+      [super draggingExited: sender];
+      return;
+    }
+
+  rect = [toolbarView bounds];
+  rect.origin.x += 3;
+  rect.origin.y += 2;
+  rect.size.width -= 5;
+  rect.size.height -= 5;
+ 
+  rect.origin.x --;
+  rect.size.width ++;
+  rect.size.height ++;
+
+  [[self window] disableFlushWindow];
+  [self displayRect: 
+	  [toolbarView convertRect: rect
+			    toView: self]];
+  [[self window] enableFlushWindow];
+  [[self window] flushWindow];
+}
+
+- (NSDragOperation) draggingUpdated: (id<NSDraggingInfo>)sender
+{
+  NSPoint loc = [sender draggingLocation];
+  NSRect rect = [toolbarView bounds];
+  NSPasteboard	*dragPb;
+  NSArray	*types;
+  
+  dragPb = [sender draggingPasteboard];
+  types = [dragPb types];
+  loc = [toolbarView convertPoint: loc
+			 fromView: nil];
+
+  if ([types containsObject: GormLinkPboardType] == YES)
+    {
+      return [super draggingUpdated: sender];
+    }
+
+  rect.origin.x += 3;
+  rect.origin.y += 2;
+  rect.size.width -= 5;
+  rect.size.height -= 5;
+
+  if (NSMouseInRect(loc, [toolbarView bounds], NO) == NO)
+    {
+      [[self window] disableFlushWindow];
+      rect.origin.x --;
+      rect.size.width ++;
+      rect.size.height ++;
+      [self displayRect: 
+	      [toolbarView convertRect: rect
+				toView: self]];
+      [[self window] enableFlushWindow];
+      [[self window] flushWindow];
+      return NSDragOperationNone;
+    }
+  else
+    {
+      [toolbarView lockFocus];
+      
+      [[NSColor darkGrayColor] set];
+      NSFrameRectWithWidth(rect, 2);
+      
+      [toolbarView unlockFocus];
+      [[self window] flushWindow];
+      return NSDragOperationCopy;
+    }
+}
+
 - (BOOL) performDragOperation: (id<NSDraggingInfo>)sender
 {
   NSPasteboard *pb = [sender draggingPasteboard];
-  
+  NSLog(@"Entered2");
   // Handle connection drags
   if ([pb availableTypeFromArray: [NSArray arrayWithObject: GormLinkPboardType]])
     {
+      NSLog(@"Success2");
       // The connection will be handled by the document
       return YES;
     }
