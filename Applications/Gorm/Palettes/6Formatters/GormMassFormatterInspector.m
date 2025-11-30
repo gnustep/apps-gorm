e713b606508a0bf4134ed62f9473a1881025cdda
Gregory John Casamento <greg.casamento@gmail.com>
2025-11-30 17:50:14 -0500

    Add inspector classes

---
+++ Applications/Gorm/Palettes/6Formatters/GormMassFormatterInspector.m
@@ -0,0 +1,37 @@
+/* Implementation of class GormMassFormatterInspector
+   Copyright (C) 2025 Free Software Foundation, Inc.
+   
+   By: Gregory John Casamento
+   Date: 30-11-2025
+
+   This file is part of GNUstep.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110 USA.
+*/
+
+#import "GormMassFormatterInspector.h"
+
+@implementation GormMassFormatterInspector
+
+- (id) init
+{
+  if ([super init] == nil)
+    return nil;
+
+  return self;
+}
+
+@end
---
+++ Applications/Gorm/Palettes/6Formatters/GormPersonNameComponentsFormatterInspector.h
@@ -0,0 +1,36 @@
+/* Definition of class GormPersonNameComponentsFormatterInspector
+   Copyright (C) 2025 Free Software Foundation, Inc.
+
+   By: Gregory John Casamento
+   Date: 30-11-2025
+
+   This file is part of GNUstep.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110 USA.
+*/
+
+#ifndef _GormPersonNameComponentsFormatterInspector_h_INCLUDE
+#define _GormPersonNameComponentsFormatterInspector_h_INCLUDE
+
+#import <InterfaceBuilder/IBInspector.h>
+
+@interface GormPersonNameComponentsFormatterInspector : IBInspector
+{
+}
+
+@end
+
+#endif	/* _GormPersonNameComponentsFormatterInspector_h_INCLUDE */
