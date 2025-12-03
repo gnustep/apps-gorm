fd1289e31ae67e0444559bdeda5c367b54c6edbd
Gregory John Casamento <greg.casamento@gmail.com>
2025-12-02 19:32:53 -0500

    Add new gorm file and inspector

--- Applications/Gorm/Palettes/6Formatters/GormLengthFormatterInspector.m
+++ Applications/Gorm/Palettes/6Formatters/GormLengthFormatterInspector.m
@@ -1,26 +1,4 @@
-/* Implementation of class GormLengthFormatterInspector
-   Copyright (C) 2025 Free Software Foundation, Inc.
-   
-   By: Gregory John Casamento
-   Date: 30-11-2025
-
-   This file is part of GNUstep.
-   
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
-   
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-   
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free
-   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110 USA.
-*/
+/* All rights reserved */
 
 #import "GormLengthFormatterInspector.h"
 
@@ -40,9 +18,62 @@
   return self;
 }
 
+- (void) revert: (id)sender
+{
+  NSLengthFormatter *formatter = (NSLengthFormatter *)[object formatter];
+  
+  if (formatter == nil)
+    return;
+  
+  // Get current values from formatter and update UI
+  
+  // Set unit style popup
+  NSFormattingUnitStyle style = [formatter unitStyle];
+  [unitStyle selectItemWithTag: (NSInteger)style];
+  
+  // Set person height use checkbox
+  BOOL heightUse = [formatter isForPersonHeightUse];
+  [forPersonHeightUse setState: heightUse ? NSOnState : NSOffState];
+  
+  // Set sample input to a default value (1.75 meters)
+  [sampleInput setDoubleValue: 1.75];
+  
+  // Generate sample output
+  NSString *sample = [formatter stringFromMeters: 1.75];
+  [sampleOutput setStringValue: sample ? sample : @""];
+  
+  [super revert: sender];
+}
+
 - (void) ok: (id)sender
 {
-  // TODO: Implement saving formatter properties
+  NSLengthFormatter *formatter = (NSLengthFormatter *)[object formatter];
+  
+  if (formatter == nil)
+    return;
+  
+  // Set unit style from popup
+  if (sender == unitStyle || sender == self)
+    {
+      NSFormattingUnitStyle style = (NSFormattingUnitStyle)[[unitStyle selectedItem] tag];
+      [formatter setUnitStyle: style];
+    }
+  
+  // Set person height use from checkbox
+  if (sender == forPersonHeightUse || sender == self)
+    {
+      BOOL heightUse = ([forPersonHeightUse state] == NSOnState);
+      [formatter setForPersonHeightUse: heightUse];
+    }
+  
+  // Update sample output when any control changes
+  if (sender == unitStyle || sender == forPersonHeightUse || sender == sampleInput || sender == self)
+    {
+      double meters = [sampleInput doubleValue];
+      NSString *sample = [formatter stringFromMeters: meters];
+      [sampleOutput setStringValue: sample ? sample : @""];
+    }
+  
   [super ok: sender];
 }
 
