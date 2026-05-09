511808fec923ea8a9042876878b88312ae2ef993
Gregory John Casamento <greg.casamento@gmail.com>
2026-05-09 10:21:51 -0400

    Add view attributes inspector so that we have fallback inspector instead of the generic one

---
+++ GormCore/GormViewAttributesInspector.h
@@ -0,0 +1,24 @@
+/* All rights reserved */
+
+#ifndef GormViewAttributesInspector_H_INCLUDE
+#define GormViewAttributesInspector_H_INCLUDE
+
+#import <InterfaceBuilder/InterfaceBuilder.h>
+
+@interface GormViewAttributesInspector : IBInspector
+{
+  IBOutlet id height;
+  IBOutlet id identifier;
+  IBOutlet id flipped;
+  IBOutlet id opaque;
+  IBOutlet id tag;
+  IBOutlet id theClass;
+  IBOutlet id width;
+  IBOutlet id xpos;
+  IBOutlet id ypos;
+}
+
+
+@end
+
+#endif // GormViewAttributesInspector_H_INCLUDE
---
+++ GormCore/GormViewAttributesInspector.m
@@ -0,0 +1,130 @@
+/* All rights reserved */
+
+#import "GormViewAttributesInspector.h"
+
+
+@implementation NSView (IBObjectAdditions)
+- (NSString *) inspectorClassName
+{
+  return @"GormViewAttributesInspector";
+}
+@end
+
+@implementation GormViewAttributesInspector
+
+- init
+{
+  self = [super init];
+  if (self != nil)
+    {
+      NSBundle *bundle = [NSBundle bundleForClass: [self class]];
+      
+      if ([bundle loadNibNamed: @"GormViewAttributesInspector" 
+			 owner: self
+	       topLevelObjects: NULL] == NO)
+	{
+	  NSLog(@"Could not open gorm GormViewAttributesInspector");
+	  NSLog(@"self %@", self);
+	  return nil;
+	}
+    }
+
+  return self;
+}
+
+- (IBAction) ok: (id)sender
+{
+  if (object == nil)
+    {
+      return;
+    }
+  else
+    {
+      NSRect frame = [object frame];
+
+      if (sender == xpos)
+	{
+	  frame.origin.x = [xpos floatValue];
+	}
+      else if (sender == ypos)
+	{
+	  frame.origin.y = [ypos floatValue];
+	}
+      else if (sender == width)
+	{
+	  frame.size.width = [xpos floatValue];
+	}
+      else if (sender == height)
+	{
+	  frame.size.height = [xpos floatValue];
+	}
+      else if (sender == theClass)
+	{
+	  // non-editable...
+	}
+      else if (sender == identifier)
+	{
+	  if ([object respondsToSelector: @selector(setIdentifier:)])
+	    {
+	      [object performSelector: @selector(setIdentifier:)
+			   withObject: [identifier stringValue]];
+	    }
+	}
+      else if (sender == tag)
+	{
+	  [object setTag: [tag integerValue]];
+	}
+
+      // reset the frame...
+      [object setFrame: frame];
+      [object setNeedsDisplay: YES];
+
+      id sv = [object superview];
+      [sv setNeedsDisplay: YES];
+    }
+}
+
+- (IBAction) revert: (id)sender
+{
+  if (object == nil)
+    {
+      return;
+    }
+  else
+    {
+      NSRect frame = [object frame];
+      NSString *className = NSStringFromClass([object class]);
+
+      [theClass setStringValue: className];
+
+      if ([object respondsToSelector: @selector(identifier)])
+	{
+	  id ident = [object performSelector: @selector(identifier)];
+	  [identifier setEditable: YES];
+	  [identifier setStringValue: ident];
+	}
+      else
+	{
+	  [identifier setEditable: NO];
+	  [identifier setStringValue: @"N/A"];
+	}
+
+      [flipped setState: ([object isFlipped] == YES) ? NSOnState : NSOffState];
+      [flipped setState: ([object isOpaque] == YES) ? NSOnState : NSOffState];
+
+      [xpos setFloatValue: frame.origin.x];
+      [ypos setFloatValue: frame.origin.y];
+      [width setFloatValue: frame.size.width];
+      [height setFloatValue: frame.size.height];
+
+      [tag setIntegerValue: [object tag]];
+    }
+}
+
+/* delegate for tag and Forms */
+- (void) controlTextDidEndEditing: (NSNotification*)aNotification
+{
+  [self ok: [aNotification object]];
+}
+
+@end
